886662118e4580d97655d589e53b6df8bef55e8c
Alan D. E. Cruse <alancruse@tutanota.com>
2020-07-15 14:44:33 +0200

    Update about me section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const languageButton = document.querySelector('#language');
 
+const headers = {portfolio: "My Portfolio"};
+
 const copyright = document.querySelector('#copyright');
 let getCurrentTime = new Date();
-console.log(getCurrentTime);
 let getCurrentYear = getCurrentTime.getUTCFullYear();
 copyright.textContent = `\u00A9 ${getCurrentYear}, Alan D. E. Cruse`;
+
